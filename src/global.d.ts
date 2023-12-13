9fd6731524ecfc1ad09fa801864716342b7404be
Katya <ekaterina.gnedenko@gmail.com>
2023-12-14 00:22:16 +0300

    finish css modules config

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,7 @@
-// declare module '*.scss' {
-//   interface IClassNames {
-//     [className: string]: string
-//   }
-//   const classNames: IClassNames;
-//   export = classNames;
-// }
-
 declare module '*.scss' {
-  const content: Record<string, string>;
-  export default content;
+  interface IClassNames {
+    [className: string]: string
+  }
+  const classNames: IClassNames;
+  export = classNames;
 }
