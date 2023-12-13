1ea65d8cc55deee5c674ffc0c2208ef3b93682d0
Katya <ekaterina.gnedenko@gmail.com>
2023-12-13 23:53:47 +0300

    configure webpack

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: true,
+          // modules: {
+          //   localIdentName: '[local]__[hash:base64:5]',
+          // }
+        },
+      },
+      "sass-loader",
+    ],
+  }
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+
+  return [typescriptLoader, cssLoader];
+}
